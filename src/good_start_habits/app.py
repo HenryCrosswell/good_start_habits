3f9b399d4fcf765bf4702c10f489725c86cde8fd
Henry Crosswell <henry.crosswell100@gmail.com>
2026-04-13 18:35:13 +0100

    feat: add list of habits

--- src/good_start_habits/app.py
+++ src/good_start_habits/app.py
@@ -1,7 +1,17 @@
-''' Front-end App - created using streamlit for interactive usability'''
+"""Front-end App - created using streamlit for interactive usability"""
 
 import streamlit as st
 
 
 st.title("good-start-habits")
 st.write("hello world")
+
+st.checkbox("SPF applied")
+st.checkbox("Vitamins & Omega-3")
+st.checkbox("Log meal")
+st.checkbox("Piano practice")
+st.checkbox("Journal entry")
+st.checkbox("Neuroscience notes")
+st.checkbox("Check to-do book")
+st.checkbox("Workout logged")
+st.checkbox("Run logged")
