98bfda25e5e02c2f5e9755c9972c9fcba51d07a0
Henry Crosswell <henry.crosswell100@gmail.com>
2026-04-13 21:36:53 +0100

    feat: add checkbox capabilities to habit tracking;

--- src/good_start_habits/app.py
+++ src/good_start_habits/app.py
@@ -6,9 +6,19 @@ import streamlit as st
 st.title("good-start-habits")
 st.write("hello world")
 
-st.checkbox("SPF applied")
-st.checkbox("Vitamins & Omega-3")
-st.checkbox("Log meal")
+"""
+Checkboxes to show completion of tasks
+
+Ticking will stop any escalation alarm
+"""
+
+
+if st.checkbox("SPF applied"):
+    st.write("Good Job!")
+if st.checkbox("Vitamins & Omega-3"):
+    st.write("Good Job!")
+if st.checkbox("Log meal"):
+    st.write("On MFP? Nice!")
 st.checkbox("Piano practice")
 st.checkbox("Journal entry")
 st.checkbox("Neuroscience notes")
