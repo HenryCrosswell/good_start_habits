3abb8e45f06ddf712f886e51b23a4b029c83acd7
Henry Crosswell <52281444+HenryCrosswell@users.noreply.github.com>
2026-04-16 11:57:45 +0100

    Feat/add habit dict (#1)
    * chore: setup streamlit basics
    
    * chore: add mkdoc integration and update to hatchling build
    
    * chore: update pre-commit.yaml
    
    * feat: add list of habits
    
    * feat: add checkbox capabilities to habit tracking;
    
    * chore: add project scope to README
    
    * chore: update README to show workflow and init config
    
    * feat: add functions that load, populate and save json
    
    * chore: define global variables, habits defined
    
    * chore: populating json file - may add to .ignore in future
    
    * chore: add last_written to json
    
    * chore: add main.yml for CI/CD, update Claude to reflect project structure
    
    ---------
    
    Co-authored-by: Henry Crosswell <henry.crosswell100@gmail.com>

---
+++ src/good_start_habits/config.py
@@ -0,0 +1,13 @@
+"""Globals are defined here"""
+
+HABITS = [
+    "SPF applied",
+    "Vitamins & Omega-3",
+    "Log meal",
+    "Piano practice",
+    "Journal entry",
+    "Neuroscience notes",
+    "Check to-do book",
+    "Workout logged",
+    "Run logged",
+]
