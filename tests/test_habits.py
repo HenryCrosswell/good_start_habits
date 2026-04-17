b6c66d32e8fd510179d06d5d24ef5ed417dc1ca5
Henry Crosswell <henry.crosswell100@gmail.com>
2026-04-17 18:14:28 +0100

    test: add tests for mark_done, create json list

--- tests/test_habits.py
+++ tests/test_habits.py
@@ -1,4 +1,4 @@
-from good_start_habits.habits import day_diff, daily_maintenance
+from good_start_habits.habits import day_diff, daily_maintenance, mark_done
 import pytest
 from typing import Any
 
@@ -11,7 +11,7 @@ list_diff_dates: list[tuple[str, str, int]] = [
     ("56", "2026-04-10", 0),
 ]
 
-list_json_dicts: list[tuple[dict[str, Any], dict[str, Any], int]] = [
+maintenance_jsons: list[tuple[dict[str, Any], dict[str, Any], int]] = [
     # ({input},
     # {expected output}),
     # day_diff
@@ -94,7 +94,7 @@ def test_day_diff(previous_date: str, current_date: str, expected: int):
     assert day_diff(previous_date, current_date) == expected
 
 
-@pytest.mark.parametrize("input, expected, day_diff", list_json_dicts)
+@pytest.mark.parametrize("input, expected, day_diff", maintenance_jsons)
 def test_daily_maintenance(
     mocker: Any, input: dict[str, Any], expected: dict[str, Any], day_diff: int
 ):
@@ -106,7 +106,18 @@ def test_daily_maintenance(
 
 
 # TODO
+json: list[tuple[dict[str, Any], dict[str, Any]]] = []
 
-# def test_mark_done():
-#     mark_done()
-#     ...
+
+@pytest.mark.parametrize(
+    "input, expected, day_diff", json
+)  # new list of jsons with updated streaks
+def test_mark_done(
+    mocker: Any, input: dict[str, Any], expected: dict[str, Any], day_diff: int
+):
+    mock_date = mocker.patch("good_start_habits.habits.date")
+    mock_date.today.return_value = "2026-04-02"
+    mocker.patch("good_start_habits.habits.save_state")
+
+    mark_done(input, "SPF applied")
+    assert input == expected
