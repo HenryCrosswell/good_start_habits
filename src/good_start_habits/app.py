95a3e7f91a516ad706871757d2adb1b53825f3f9
Henry Crosswell <henry.crosswell100@gmail.com>
2026-04-19 14:05:35 +0100

    chore: add new habit funcs to app interface

--- src/good_start_habits/app.py
+++ src/good_start_habits/app.py
@@ -1,27 +1,29 @@
 """Front-end App - created using streamlit for interactive usability"""
 
+from .habits import load_state, daily_maintenance, mark_done
 import streamlit as st
-
 
 st.title("good-start-habits")
 st.write("hello world")
 
-"""
-Checkboxes to show completion of tasks
-
-Ticking will stop any escalation alarm
-"""
-
+state_json = load_state()
+daily_maintenance(state_json)
 
 if st.checkbox("SPF applied"):
-    st.write("Good Job!")
+    mark_done(state_json, "SPF applied")
 if st.checkbox("Vitamins & Omega-3"):
-    st.write("Good Job!")
+    mark_done(state_json, "Vitamins & Omega-3")
 if st.checkbox("Log meal"):
-    st.write("On MFP? Nice!")
-st.checkbox("Piano practice")
-st.checkbox("Journal entry")
-st.checkbox("Neuroscience notes")
-st.checkbox("Check to-do book")
-st.checkbox("Workout logged")
-st.checkbox("Run logged")
+    mark_done(state_json, "Log meal")
+if st.checkbox("Piano practice"):
+    mark_done(state_json, "Piano practice")
+if st.checkbox("Journal entry"):
+    mark_done(state_json, "Journal entry")
+if st.checkbox("Neuroscience notes"):
+    mark_done(state_json, "Neuroscience notes")
+if st.checkbox("Check to-do book"):
+    mark_done(state_json, "Check to-do book")
+if st.checkbox("Workout logged"):
+    mark_done(state_json, "Workout logged")
+if st.checkbox("Run logged"):
+    mark_done(state_json, "Run logged")
