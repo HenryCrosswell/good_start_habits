11fe92e9342301b5267a3d35f87fd68f513d2b83
Henry Crosswell <52281444+HenryCrosswell@users.noreply.github.com>
2026-04-19 13:52:40 +0100

    Feat/add tests (#2)
    * feat: add tests for day_diff
    
    * test: add tests for daily_maintenance func
    
    * test: add tests for daily_maintenance func
    
    * test: add tests for mark_done, create json list
    
    * test: add test for mark done
    
    * chore: add cache
    
    * chore: add cache
    
    ---------
    
    Co-authored-by: Henry Crosswell <henry.crosswell100@gmail.com>

--- tests/test_habits.py
+++ tests/test_habits.py
@@ -1,1 +1,149 @@
-def test_day_diff(): ...
+from good_start_habits.habits import day_diff, daily_maintenance, mark_done
+import pytest
+from typing import Any
+
+### Variables init ###############################################################################
+list_diff_dates: list[tuple[str, str, int]] = [
+    ("2026-04-01", "2026-04-10", 9),
+    ("2026-04-01", "2026-05-01", 30),
+    ("2025-12-31", "2026-01-05", 5),
+    ("1", "0", 0),
+    ("56", "2026-04-10", 0),
+]
+
+maintenance_jsons: list[tuple[dict[str, Any], dict[str, Any], int]] = [
+    # ({input},
+    # {expected output}),
+    # day_diff
+    (
+        {
+            "SPF applied": {
+                "streak": 12,
+                "last_completed": "2026-04-02",
+                "done_today": True,
+            }
+        },
+        {
+            "SPF applied": {
+                "streak": 12,
+                "last_completed": "2026-04-02",
+                "done_today": True,
+            }
+        },
+        0,
+    ),
+    (
+        {
+            "SPF applied": {
+                "streak": 12,
+                "last_completed": "2026-04-08",
+                "done_today": False,
+            }
+        },
+        {
+            "SPF applied": {
+                "streak": 0,
+                "last_completed": "2026-04-08",
+                "done_today": False,
+            }
+        },
+        10,
+    ),
+    (
+        {
+            "SPF applied": {
+                "streak": 12,
+                "last_completed": "2026-04-10",
+                "done_today": True,
+            }
+        },
+        {
+            "SPF applied": {
+                "streak": 12,
+                "last_completed": "2026-04-10",
+                "done_today": False,
+            }
+        },
+        1,
+    ),
+    (
+        {
+            "SPF applied": {
+                "streak": 12,
+                "last_completed": "2026-04-10",
+                "done_today": True,
+            }
+        },
+        {
+            "SPF applied": {
+                "streak": 12,
+                "last_completed": "2026-04-10",
+                "done_today": False,
+            }
+        },
+        2,
+    ),
+]
+
+mark_done_jsons: list[tuple[dict[str, Any], dict[str, Any]]] = [
+    (
+        {
+            "SPF applied": {
+                "streak": 12,
+                "last_completed": "2026-04-02",
+                "done_today": False,
+            }
+        },
+        {
+            "SPF applied": {
+                "streak": 13,
+                "last_completed": "2026-04-03",
+                "done_today": True,
+            }
+        },
+    ),
+    (
+        {
+            "SPF applied": {
+                "streak": 8,
+                "last_completed": "2026-04-02",
+                "done_today": True,
+            }
+        },
+        {
+            "SPF applied": {
+                "streak": 8,
+                "last_completed": "2026-04-02",
+                "done_today": True,
+            }
+        },
+    ),
+]
+
+### TESTS ########################################################################################
+
+
+@pytest.mark.parametrize("previous_date, current_date, expected", list_diff_dates)
+def test_day_diff(previous_date: str, current_date: str, expected: int):
+    assert day_diff(previous_date, current_date) == expected
+
+
+@pytest.mark.parametrize("input, expected, day_diff", maintenance_jsons)
+def test_daily_maintenance(
+    mocker: Any, input: dict[str, Any], expected: dict[str, Any], day_diff: int
+):
+    mocker.patch("good_start_habits.habits.day_diff", return_value=day_diff)
+    mocker.patch("good_start_habits.habits.save_state")
+
+    daily_maintenance(input)
+    assert input == expected
+
+
+@pytest.mark.parametrize("input, expected", mark_done_jsons)
+def test_mark_done(mocker: Any, input: dict[str, Any], expected: dict[str, Any]):
+    mock_date = mocker.patch("good_start_habits.habits.date")
+    mock_date.today.return_value = "2026-04-03"
+    mocker.patch("good_start_habits.habits.save_state")
+
+    mark_done(input, "SPF applied")
+    assert input == expected
