cbb0681134eb04210b9a1dbdac559f9e836b0bbe
Henry Crosswell <henry.crosswell100@gmail.com>
2026-04-22 16:09:00 +0100

    chore: update db.py as current functionality doesnt work without flask requests

--- src/good_start_habits/db.py
+++ src/good_start_habits/db.py
@@ -2,29 +2,29 @@ import sqlite3
 from flask import g
 
 
-def init_db():
-    # Create a table
-    con = sqlite3.connect("dashboard.db")
-    cur = con.cursor()
-    cur.execute(
-        """
-    CREATE TABLE IF NOT EXISTS habits (
-    name          TEXT PRIMARY KEY,
-    streak        INTEGER NOT NULL DEFAULT 0,
-    last_completed TEXT,
-    done_today    INTEGER NOT NULL DEFAULT 0
-  """
-    )
-
-
 def get_db():
-    db = getattr(g, "_database", None)
+    db = getattr(g, "database", None)
     if db is None:
-        db = g._database = sqlite3.connect("dashboard.db")
+        db = g.database = sqlite3.connect("dashboard.db")
     return db
 
 
-def close_db(e: BaseException):
-    db = g.pop("db", None)
+def init_db():
+    db = get_db()
+    db.execute(
+        """
+        CREATE TABLE IF NOT EXISTS habits (
+        name           TEXT PRIMARY KEY,
+        streak         INTEGER NOT NULL DEFAULT 0,
+        last_completed TEXT,
+        done_today     INTEGER NOT NULL DEFAULT 0
+        );
+        """
+    )
+    db.commit()
+
+
+def close_db(e=None):
+    db = g.pop("database", None)
     if db is not None:
         db.close()
