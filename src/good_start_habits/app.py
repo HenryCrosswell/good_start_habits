dd8d9137ac51bb68be3dabd4d126225a60f7ae71
Henry Crosswell <henry.crosswell100@gmail.com>
2026-04-24 09:58:37 +0100

    chore: create basic templates for flask

--- src/good_start_habits/app.py
+++ src/good_start_habits/app.py
@@ -1,5 +1,29 @@
 """Front-end App - using streamlit for interactive usability"""
 
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/clock")
+def clock():
+    return "<p>This my clock!</p>"
+
+
+@app.route("/habits")
+def habits():
+    return "<p>These my habits!</p>"
+
+
+@app.route("/budget")
+def budget():
+    return "<p>This my budget!</p>"
+
 
 # @st.fragment(run_every=1)  # reruns only this func every second
 # def clock():
